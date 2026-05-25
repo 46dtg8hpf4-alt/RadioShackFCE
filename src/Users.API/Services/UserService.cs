7748d52076eac89e470f073f3d7583947762a2b4
Francisco Regoli Freire <fregolifreire21@gmail.com>
2026-05-25 18:05:41 -0300

    Implementacion de capa de servicios

---
+++ src/Users.API/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Users.API.DTOS;
+using Users.API.Exceptions;
+using Users.API.Models;
+
+namespace Users.API.Services
+{
+    public class UserService : IUserService
+    { 
+        private static readonly List<Users.API.Models.Users> _usersDb = new List<Users.API.Models.Users>();
+
+        public async Task<UserResponseDTO> RegisterAsync(RegisterRequestDTO request)
+        {
+            var exists = _usersDb.Any(u => u.Email.Equals(request.Email, StringComparison.OrdinalIgnoreCase));
+            if (exists)
+            {
+                throw new BusinessRuleException("USR-001", "El correo electrónico ya se encuentra registrado.");
+            }
+
+            string hashedPassword = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(request.Password));
+
+            var newUser = new Users.API.Models.Users
+            {
+                Id = Guid.NewGuid(),
+                Nombre = request.Nombre,
+                Apellido = request.Apellido,
+                Email = request.Email,
+                PasswordHash = hashedPassword,
+                FechaRegistro = DateTime.UtcNow,
+                Activo = true,
+                IntentosFallidos = 0
+            };
+
+            _usersDb.Add(newUser);
+
+            return new UserResponseDTO
+            {
+                Id = newUser.Id,
+                Nombre = newUser.Nombre,
+                Apellido = newUser.Apellido,
+                Email = newUser.Email,
+                FechaRegistro = newUser.FechaRegistro,
+                Activo = newUser.Activo
+            };
+        }
+
+        public async Task<UserResponseDTO> LoginAsync(LoginRequestDTO request)
+        {
+            return new UserResponseDTO();
+        }
+    }
+}
