b7e53a75a004e7f3efaac06275f838b071d1dbf7
Bruno Anes POLIMI <rancano.bruno@gmail.com>
2026-05-25 16:37:13 -0300

    Actualización de campos obligatorios de Product

--- src/Products.API/Models/Product.cs
+++ src/Products.API/Models/Product.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Products.API.Models
 {
     public class Product
@@ -6,21 +8,27 @@ namespace Products.API.Models
         public int Id { get; set; }
 
         // Nombre del producto
+        [Required]
         public string Nombre { get; set; }
 
         // Descripción del producto
+        [Required]
         public string Descripcion { get; set; }
 
         // Precio del producto
+        [Range(1, 999999)]
         public decimal Precio { get; set; }
 
         // Cantidad disponible en stock
+        [Range(0, 9999)]
         public int Stock { get; set; }
 
         // Categoría del producto
+        [Required]
         public string Categoria { get; set; }
 
         // Marca del producto
+        [Required]
         public string Marca { get; set; }
 
         // Constructor vacío
